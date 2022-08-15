da9e56a4bec58ed77ca22fb90977ce2b66bfd00a
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-08-15 13:41:50 -0300

    Inicio do projeto.

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GifAppPro from './GifAppPro'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifAppPro />
+  </React.StrictMode>
+)
