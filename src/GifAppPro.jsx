66bf02fbf077c6c5caa2f619616bc47ecd3c0022
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-08-15 19:17:45 -0300

    feat: comunicação entre componentes GifAppPro e AddCategory adicionado.

--- src/GifAppPro.jsx
+++ src/GifAppPro.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react"
 import AddCategory from "./components/AddCategory"
 
-AddCategory
-
 export default function GifAppPro() {
-  
+  const [ categories, setCategories ] = useState([ "Batman", "Superman" ])
+
 
   return (
     <>
       <h1>Gif App Pro</h1>
 
-      <AddCategory />
+      <AddCategory setCategory={ setCategories }/>
+
+      <ol>
+        {categories.map(category => {
+          return(
+            <li key={category}>{ category }</li>
+          )
+        })}
+      </ol>
+      
 
 
     </>
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react"
 
-export default function AddCategory() {
-  const [ valor, setValor ] = useState([ "Batman", "Superman" ])
+export default function AddCategory( { setCategory } ) {
+  const [inputValue, setInputValue] = useState()
+
+  const onInputChange = ( e ) => {
+    setInputValue( e )
+  }
+
+  const onSubmit = ( event ) => {
+    event.preventDefault()
+    if( inputValue.trim().length <= 0 ) return;
+
+    setCategory(categories => [ inputValue, ...categories ])
+    setInputValue("")
+  }
 
   return (
-    <form>
+    <form onSubmit={ onSubmit }>
       
-      <input type="text" />
-
-      <ol>
-        <li></li>
-      </ol>
+      <input
+        type="text"
+        placeholder="Procurar gifs"
+        value={ inputValue }
+        onChange={e => onInputChange(e.target.value)}
+      />
 
     </form>
   )
