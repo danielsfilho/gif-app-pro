c8bcccdc7527f526c011c8d830e306734b00f630
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-08-26 18:01:40 -0300

    feat: submit do formulário testado

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -3,14 +3,31 @@ import { AddCategory } from "../../src/components/AddCategory";
 
 
 describe('Testando o componente AddCategory', () => { 
-  
+  const inputValue = 'Saitama';
+
   test('deve mudar o valor do campo de texto', () => { 
     
     render( <AddCategory onNewCategory={ () => {} } /> );
     const input = screen.getByRole('textbox');
 
-    fireEvent.input( input, { target: { value: 'Saitama' } });
+    fireEvent.input( input, { target: { value: inputValue } });
 
     expect( input.value ).toBe( 'Saitama' );
+  });
+
+  test('deve chamar o onNewCategory se o input tem um valor', () => { 
+    
+    
+    // TODO: ???
+    
+    render( <AddCategory onNewCategory={ () => {} } /> );
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input( input, { target: { value: inputValue } });
+    fireEvent.submit( form );
+
+    expect( input.value ).toBe( '' );
   });
 });
