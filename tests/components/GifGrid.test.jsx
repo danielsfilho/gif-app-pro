7a370acd6f69e962f3bec21c5981d09330ec7546
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-08-26 21:29:10 -0300

    feat: realizado o mock do custom hook useFetchGifs

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
 
-import { GifGrid } from '../../src/components'
+import { GifGrid } from '../../src/components';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Testes no componente GifGrid ', () => {
 
@@ -8,11 +11,36 @@ describe('Testes no componente GifGrid ', () => {
   
   test('deve mostrar o loading inicialmente', () => { 
     
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    })
+
     render( <GifGrid category={ category } /> );
     expect( screen.getByText( category ) );
   });
 
   test('deve mostrar itens quando carregue as imagens useFetchGifs', () => { 
-    second 
-  })
+    
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'Saitama',
+        url: 'https://localhost/saitama.jpg'
+      },
+      {
+        id: '123',
+        title: 'Goku',
+        url: 'https://localhost/goku.jpg'
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    });
+
+    render( <GifGrid category={ category } /> );
+    expect( screen.getAllByRole('img').length ).toBe(2);
+  });
 });
